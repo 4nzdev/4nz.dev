f09ebec9013ac37fbc771c7edf7de17d5cbea047
Anti <164034497+4nzdev@users.noreply.github.com>
2026-01-14 11:47:41 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 let clix = 0;
 let to = setTimeout();
-let to2 = setTimeout();
 
 function clik() {
   clix++
   elem("counter").innerHTML = clix + " clix :3";
 
   clearTimeout(to);
-  clearTimeout(to2);
   
   to = setTimeout(() => {
     elem("counter").innerHTML = clix + " clix :<";
-  }, 3000)
-  
-  to2 = setTimeout(() => {
-    elem("counter").innerHTML = clix + " clix >:O";
-  }, 10000)
-
+  }, 5000)
   
 }
 
